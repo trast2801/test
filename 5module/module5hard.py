16526a67ace267ed9933aae78a649b163928645b
@trast2801 <trast2801@yandex.ru>
2024-06-20 01:14:36 +0300

    переделал под ТЗ завтра почистить код

--- 5module/module5hard.py
+++ 5module/module5hard.py
@@ -1,3 +1,4 @@
+import time
 from os import remove
 from random import random, randint
 
@@ -11,7 +12,7 @@ class Video:
 
 class User:
     count = 0
-    def __init__(self, nickname : str, age : int, password : int):
+    def __init__(self, nickname: str, age: str, password: str):
         self.nickname = nickname
         self.age = age
         self.password = hash(password)
@@ -21,14 +22,139 @@ class UrTube:
 
     def __init__(self, users: [], videos: [], current_user: User):
         self.users = users
-        self.video = videos
+        self.videos = videos
         self.current_user = current_user
+
+    def log_in(self, login: str, password: str):
+        password = hash(password)
+        for k in self.users:
+            if k.nickname == login and k.password == password:
+               self.current_user = k
+        return
+    def register(self, nickname: str, password: str, age: str):
+        for k in self.users:
+            if k.nickname == nickname:
+                print('\nПользователь существует ')
+                return False
+        user = User(nickname, age, password)
+        self.users.append(user)
+        self.current_user = user
+        return True
+
+    def show_users(self):
+        for i in self.users:
+            print(i.nickname, i.age, i.password)
+
+    def fill_bd(self):
+        #заполнение БД тестовыми данными
+        for i in range (0,10):
+            self.users.append(User('support' + str(i), randint(15, 90), 'passwd'+ str(randint(1,100))))
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *args):
+        for k in args:
+            flag = False
+            for l in self.videos:
+                if l.title == k.title:
+                    flag = True
+                    break
+            if flag:
+                continue
+            else:
+                self.videos.append(k)
+
+    def get_videos(self, substr: str):
+        spisok = []
+        for k in self.videos:
+            if k.title.lower().find(substr.lower()) != -1:
+                spisok.append(k.title)
+        return spisok
+
+    def list_video(self):
+        for k in self.videos:
+            print(k.title)
+
+    def watch_video(self, title):
+        for k in self.videos:
+            if title == (k.title):
+
+                if self.current_user == None:
+                    print('Войдите в аккаунт, чтобы смотреть видео')
+                    return
+                if int (self.current_user.age) < 18 and (k.adult_mode == True):
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                    return
+                else:
+                    for i in range(0, k.duration):
+                        k.time_now = i
+                        time.sleep(1)
+                        print(k.time_now, end=" "),
+                    k.time_now = 0
+
+
+if __name__ == '__main__':
+    hill = UrTube([],[],None)
+    hill.fill_bd()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+    v3 = Video('Лучший язык программирования 2024 года', 200)
+    v4 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+    # Добавление видео
+    hill.add(v1, v2, v3, v4)
+#    hill.list_video()
+    print(hill.get_videos('лучший'))
+    print(hill.get_videos('ПРОГ'))
+
+    hill.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(hill.current_user)
+
+    hill.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(hill.current_user)
+    #hill.log_out()
+
+    #hill.watch_video('Для чего девушкам парень программист?')
+
+    #hill.register('vasya_pupkin', 'lolkekcheburek', 64)
+    hill.watch_video('Для чего девушкам парень программист?')
+
+    #for i in hill.get_videos('парень'):
+    #    print(i)
+
+    while True:
+        choice = input('\nДобро пожаловать в Свой YouTube\n'
+                   '___________________________________\n'
+                   'Нажмите 1 - для регистрации \n'
+                   'Нажмите 2 - входа\n'
+                   'Нажмите 3 - если хотите удалить пользователя\n'
+                   'Нажмите 4 - для получения списка пользователей\n'
+                   'Нажмите 5 - для выхода из Свой YouTube'    
+                   'Нажмите 0 - если хотите закончить\n')
+        if choice == '0':
+            print('До свидания')
+            exit()
+        if choice == '1':
+            login  = input('Введите логин:')
+            passwd = input('Введите пароль:')
+            age    = input('Введите Ваш возраст:')
+            if hill.register(login, passwd, age):
+                print ('Регистрация прошла успешно, Вы в системе')
+            else:
+                continue
+        if choice == '2':
+            hill.log_in(input('\nВведите логин:'), input('Введите пароль:'))
+        if choice == 5:
+            hill.log_out()
+        if choice == '4':
+            hill.show_users()
+
 
 class Database:
 
     def __init__(self, user: User):
         self.data = []
-
 
     def add_user(self, user: User):
         #self.data[username] = hash(password)
@@ -63,7 +189,7 @@ class Database:
            k = User('support' + str(k), randint(15, 90), hash(str(random() * 10) + ' password '))
            self.add_user(k)
         self.add_user(User("konst", 45, '123'))
-    def login(self, username, password):
+    def log_in(self, username, password):
         for k in self.data:
             if k.nickname == username:
                 if k.password == hash(password):
@@ -94,7 +220,7 @@ def reg(exemplar : Database):
             else:
                 print('Пароли не совпадают, начните сначала')
 
-
+'''
 if __name__ == '__main__':
     user = User('www',23, 'dddf')
     exemplar = Database(user)
@@ -116,7 +242,7 @@ if __name__ == '__main__':
             username = input('\n Введите логин:')
             if exemplar.find_user(username): #оптимизировать функцию поиска при регистраии и лоигне (использовать одну)
                password = input('\n Введите пароль:')
-               exemplar.login(username, password)
+               exemplar.log_in(username, password)
             else:
                 print('Такого пользователя в базе не зарегистрировано')
         if ss == '3':
@@ -127,7 +253,7 @@ if __name__ == '__main__':
             exemplar.spisok_all_users_in_BD()
         else:
             continue
-
+'''
 
 
 
