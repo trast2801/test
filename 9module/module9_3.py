a96b16c41c31a3c1df4033d719406e91769c804d
@trast2801 <trast2801@yandex.ru>
2024-07-01 17:56:26 +0300

    все переделал

--- 9module/module9_3.py
+++ 9module/module9_3.py
@@ -1,21 +1,28 @@
 class EvenNumbers:
     def __init__(self, start=0, end=1):
-        if end < start:
-            end = start + 1
-        self.start = start-1
-        self.end = end
-        #self.i = start
-    def __iter__(self):
-        return self
-    def __next__(self):
-        while True:
-            #self.i += 1
-            self.start += 1
-            if self.start == self.end:
-                raise StopIteration()
-            if (self.start % 2 == 0):
-                return (self.start)
 
-spisok = EvenNumbers(10, 25)
-for i in spisok:
-    print(i)
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        self.i = self.start - 1
+        return self
+
+    def __next__(self):
+        if self.i % 2:
+            self.i += 1
+        else:
+            self.i += 2
+        if self.i >= self.end:
+            raise StopIteration
+        return self.i
+
+
+
+en = EvenNumbers(10, 25)
+for i in en:
+     print(i)
+print('----------------------------')
+for i in en:
+     print(i)
+
