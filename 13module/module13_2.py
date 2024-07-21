d98d7e8338b68fe3be57fba0639db9273eca0816
@trast2801 <trast2801@yandex.ru>
2024-07-21 08:34:41 +0300

    на проверку

--- 13module/module13_2.py
+++ 13module/module13_2.py
@@ -1,8 +1,10 @@
 
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token='')
-dp = Dispatcher(bot)
+API ='7354419811:AAEk3KUNZVBZTXqT3mGGq5OKCnS79XkrFeI'
+bot = Bot(token=API)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async  def start(message):
