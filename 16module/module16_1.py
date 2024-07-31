cd6d403d24348935d2213c1c5643b0e8487480d0
@trast2801 <trast2801@yandex.ru>
2024-07-31 20:08:34 +0300

    поправил

--- 16module/module16_1.py
+++ 16module/module16_1.py
@@ -1,19 +1,27 @@
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 @app.get("/")
-async def welcom():
+async def head_page():
     return "Главная страница"
 
 @app.get("/user/admin")
-async def welcom():
+async def admin_enter():
     return ("Вы вошли как администратор")
 @app.get("/user/{user_id}")
-async  def one(user_id):
+async  def user_enter(user_id):
     return (f'Вы вошли как пользователь № {user_id}')
 
 @app.get("/user")
-async def vsor(username : str, age : str):
+async def info_about_user(username : str, age : int):
     return (f"Информация о пользователе. Имя: {username}, Возраст:{age}")
+
+@app.get("/user/{username}")
+async def user_name(username : str = Path(min_length = 3, max_length=15, description =
+                                "Введите ваше имя ")) -> dict:
+
+    return{"message": f'привет {username} {id}'}
+
