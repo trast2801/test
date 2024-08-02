65060c90e9e0781f5c32d46811cb8240fee391cb
@trast2801 <trast2801@yandex.ru>
2024-08-02 10:05:09 +0300

    на проверку

--- 17module/app/main.py
+++ 17module/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from routers import  task, user
+from routers import task, user
 
 app = FastAPI()
+
 
 @app.get("/")
 async def welcome():
     return {"message": "Welcom to Taskmnager"}
+
 
 app.include_router(user.router)
 app.include_router(task.router)
