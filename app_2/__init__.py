e6b018f1dcc02b5f77067ed2171de3f5bc707696
@trast2801 <trast2801@yandex.ru>
2024-08-03 20:21:21 +0300

    на проверку

---
+++ app_2/__init__.py
@@ -0,0 +1,2 @@
+#from app_2.models.user import User
+#from app_2.models.task import Task
--- app_2/models/user.py
+++ app_2/models/user.py
@@ -1,16 +1,21 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
-from app.backend.db import Base
+from app_2.backend.db import Base
 from sqlalchemy.schema import CreateTable
 
 class User(Base):
     __tablename__ = "users"
+    #__table_args__ = {'keep_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     firstname = Column(String)
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String, unique=True, index=True)
+    #task_id = Column(Integer, ForeignKey=('task.id'))
 
+    tasks = relationship('Task', back_populates='user')
 
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
 #print(CreateTable(Task.__table__))
