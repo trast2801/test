735fcd980731073361d1f5c1d9f152ff29033c8d
@trast2801 <trast2801@yandex.ru>
2024-08-03 20:22:33 +0300

    на проверку

--- app_2/models/task.py
+++ app_2/models/task.py
@@ -16,7 +16,7 @@ class Task(Base):
     completed = Column(Boolean, default=False)
     user_id = Column(Integer,ForeignKey('users.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
-    is_active = Column(Boolean, default=True)
+
 
     user = relationship('User', back_populates='tasks')
 
